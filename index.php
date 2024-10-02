ae725125112ce3c7b1ac9fe405e4f89bb96af7c6
Coder & AccoTax <codernaccotax@gmail.com>
2024-10-02 22:49:14 +0530

    Added New Row

--- index.php
+++ index.php
@@ -9,37 +9,76 @@
 </head>
 
 <body>
-    <h1>Welcome</h1>
     <div class="container">
         <form action="">
             <div class="d-flex flex-column flex-md-row">
+
                 <!-- Entering student name -->
-                <div class="col col-md-4">
+                <div class="col col-md-4 mt-2 ">
                     <div class="form-group">
-                        <label for="student-name">Student Name</label>
+                        <label for="student-name"><strong>Student Name</strong></label>
                         <input type="text" class="form-control" id="student-name" aria-describedby="studentNameHelp" placeholder="Enter your name">
                         <small id="studentNameHelp" class="form-text text-muted">Enter your full name</small>
                     </div>
                 </div>
                 <!-- Entering student dob -->
-                <div class="col col-md-4">
+                <div class="col  col-md-4 mt-2">
                     <div class="form-group">
-                        <label for="dob">Date of birth</label>
+                        <label for="dob"><strong>Date of birth</strong></label>
                         <input type="date" class="form-control" id="dob" aria-describedby="studentDOBHelp" placeholder="Enter your Date of birth">
                         <small id="studentDOBHelp" class="form-text text-muted">Enter your Date of birth</small>
                     </div>
                 </div>
                 <!-- Entering student sex -->
-                <div class="col col-md-4">
-                    <div class="form-group">
-                        <label for="dob">Sex</label>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                            <label class="form-check-label" for="inlineRadio1">1</label>
+                <div class="container mt-md-4 height-md-8 mt-2">
+                    <label><strong>SEX</strong></label>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="female" value="female">
+                                <label class="form-check-label" for="female">
+                                    Female
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="male" value="male">
+                                <label class="form-check-label" for="male">
+                                    Male
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="others" value="others">
+                                <label class="form-check-label" for="others">
+                                    Others
+                                </label>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="d-flex flex-column flex-md-row">
+                <div class="col col-md-4 mt-2">
+                    <div class="form-group">
+                        <label for="guardian-name"><strong>Guardian Name</strong></label>
+                        <input type="text" class="form-control" id="guardian-name" aria-describedby="guardianNameHelp" placeholder="Enter your name">
+                        <small id="guardianNameHelp" class="form-text text-muted">Enter Your Guardian Name Here</small>
+                    </div>
+                </div>
+                <div class="col  col-md-4 mt-2">
+                    <div class="form-group">
+                        <label for="contact"><strong>Contact</strong></label>
+                        <input type="text" class="form-control" id="contact" aria-describedby="studentContactHelp" placeholder="Enter Your Contact Number">
+                        <small id="studentContactHelp" class="form-text text-muted">Enter Your Contact Number</small>
+                    </div>
+                </div>
+                <div class="container mt-md-4 height-md-8 mt-2">
+                </div>
+
 
 
         </form>
